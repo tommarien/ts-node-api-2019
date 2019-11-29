eddb8a1528caa2bf290b8c1e57632d4bb1b1691d
Tom Marien <tommarien@gmail.com>
2019-11-29 22:35:15 +0100

    test(mocks): Added all main logging functions to pino mock

--- src/__mocks__/pino.ts
+++ src/__mocks__/pino.ts
@@ -1,7 +1,12 @@
-export const error = jest.fn();
+const mockLogger = {
+  fatal: jest.fn(),
+  error: jest.fn(),
+  warn: jest.fn(),
+  info: jest.fn(),
+  debug: jest.fn(),
+  trace: jest.fn(),
+};
 
-const pino = jest.fn().mockImplementation(() => ({
-  error,
-}));
+const pino = jest.fn().mockImplementation(() => mockLogger);
 
 export default pino;
