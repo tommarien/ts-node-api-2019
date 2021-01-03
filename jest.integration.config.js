b8bffc40c95345700002e2a68216580afaf74a9e
Tom Marien <tommarien@gmail.com>
2021-01-03 21:17:42 +0100

    revert unneeded changes

--- jest.integration.config.js
+++ jest.integration.config.js
@@ -1,9 +1,16 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/?(*.)+(test).[jt]s?(x)'],
-  testPathIgnorePatterns: ['dist'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  roots: ['<rootDir>/src'],
-  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+  testMatch: [
+    "**/?(*.)+(test).[jt]s?(x)"
+  ],
+  testPathIgnorePatterns: ["dist"],
+  setupFilesAfterEnv: [
+    "<rootDir>/jest.setup.js"
+  ],
+  roots:['<rootDir>/src'],
+  watchPlugins: [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname"
+  ]
 };
--- jest.unit.config.js
+++ jest.unit.config.js
@@ -1,9 +1,16 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/?(*.)+(spec).[jt]s?(x)'],
-  testPathIgnorePatterns: ['dist'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  roots: ['<rootDir>/src'],
-  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+  testMatch: [
+    "**/?(*.)+(spec).[jt]s?(x)"
+  ],
+  testPathIgnorePatterns: ["dist"],
+  setupFilesAfterEnv: [
+    "<rootDir>/jest.setup.js"
+  ],
+  roots:['<rootDir>/src'],
+  watchPlugins: [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname"
+  ]
 };
