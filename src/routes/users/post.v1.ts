cd690e5d82bcdb78bd99a18c0eec108d12ddfb5a
Tom Marien <tommarien@gmail.com>
2021-01-03 21:49:24 +0100

    fix(api): add missing error handling

--- src/routes/users/post.v1.ts
+++ src/routes/users/post.v1.ts
@@ -5,19 +5,23 @@ import { UserRequestBodyV1 } from '../../schemas/types/user.request.body.v1';
 import userRepository from '../../services/userRepository';
 import { mapToResource, UserResource } from './userResource';
 
-const postUserV1: RequestHandler<unknown, UserResource, UserRequestBodyV1> = async (req, res): Promise<void> => {
-  const {
-    body: { birthDate, ...rest },
-  } = req;
+const postUserV1: RequestHandler<unknown, UserResource, UserRequestBodyV1> = async (req, res, next): Promise<void> => {
+  try {
+    const {
+      body: { birthDate, ...rest },
+    } = req;
 
-  const user = await userRepository.create({
-    ...rest,
-    ...(birthDate ? { birthDate: new Date(birthDate) } : {}),
-  });
+    const user = await userRepository.create({
+      ...rest,
+      ...(birthDate ? { birthDate: new Date(birthDate) } : {}),
+    });
 
-  const resBody: UserResource = mapToResource(user);
+    const resBody: UserResource = mapToResource(user);
 
-  res.send(resBody);
+    res.send(resBody);
+  } catch (e) {
+    next(e);
+  }
 };
 
 export default [validate({ body: userBodySchema }), postUserV1];
