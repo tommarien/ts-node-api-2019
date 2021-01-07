af19b9bfbdd390740b8f8bef6322066e72cb4a8f
Tom Marien <tommarien@gmail.com>
2021-01-07 23:26:15 +0100

    refactor(test): no more separation between unit or int test

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,13 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: [
-    "**/?(*.)+(spec).[jt]s?(x)"
+    "**/?(*.)+(spec|test).[jt]s?(x)"
   ],
   testPathIgnorePatterns: ["dist"],
   setupFilesAfterEnv: [
     "<rootDir>/jest.setup.js"
   ],
-  roots:['<rootDir>/src'],
+  roots: ['<rootDir>/src'],
   watchPlugins: [
     "jest-watch-typeahead/filename",
     "jest-watch-typeahead/testname"
