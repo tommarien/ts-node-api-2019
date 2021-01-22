be3f758d88c785d18c2466155bbdd4be209d2b4e
Tom Marien <tommarien@gmail.com>
2021-01-22 23:26:53 +0100

    refactor: rename routerInstaller to routeInstaller

--- src/@types/api.d.ts
+++ src/@types/api.d.ts
@@ -5,7 +5,7 @@ import { Request, Response, Router } from 'express';
 
 export type Environment = 'local' | 'dev' | 'sta' | 'pro';
 
-export type RouterInstaller = (router: Router) => void;
+export type RouteInstaller = (router: Router) => void;
 
 export type Mapper<S, T> = (src: Readonly<S>) => T;
 
--- src/api/routes/users/index.ts
+++ src/api/routes/users/index.ts
@@ -1,9 +1,9 @@
-import { RouterInstaller } from '../../../@types/api';
+import { RouteInstaller } from '../../../@types/api';
 import deleteV1 from './delete.v1';
 import getV1 from './get.v1';
 import postV1 from './post.v1';
 
-export const userRouterInstaller: RouterInstaller = (router) => {
+export const userRouterInstaller: RouteInstaller = (router) => {
   router //
     .route('/v1/users')
     .post(postV1);
